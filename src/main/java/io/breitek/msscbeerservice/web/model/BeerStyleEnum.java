d1dda248a909c86a5be67ed011652219bfc68951
jbreitzman <jbreitzman@gmail.com>
2021-01-09 12:23:53 -0500

    Create Data Model for Beer Service. Closes #1

---
+++ src/main/java/io/breitek/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,14 @@
+package io.breitek.msscbeerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER,
+    PILSNER,
+    STOUT,
+    GOSE,
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE,
+    SAISON
+}
